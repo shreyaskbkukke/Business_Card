fb05ce08a83236b781a9c989c13d6a5894495b58
Shreyas KB <shreyaskb333@gmail.com>
2024-02-06 22:18:50 +0530

    2.1.0

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,27 +1,35 @@
 package com.example.businesscard
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +40,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFFD2E8D4)
                 ) {
                     BusinessCardApp()
                 }
@@ -43,7 +51,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCardApp(){
-    GeneralDetails()
+    GeneralDetails(
+        title = stringResource(R.string.name),
+        line = stringResource(R.string.job_line)
+    )
     ContactDetails(
         mobileNumber = stringResource(R.string.mobile_number),
         username = stringResource(R.string.username),
@@ -52,10 +63,42 @@ fun BusinessCardApp(){
 }
 
 @Composable
-fun GeneralDetails(){
-    Column {
-        Image(painter = , contentDescription = )
+fun GeneralDetails(
+    title: String,
+    line: String
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Spacer(modifier = Modifier.padding(top = 125.dp))
+        Box(modifier = Modifier.size(150.dp)){
+            Image(
+                painterResource(id = R.drawable._0230630_141612_2),
+                contentDescription = "Shreyas",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
     }
+    Text(
+
+        modifier = Modifier
+            .padding(top = 280.dp),
+        text = title,
+        fontSize = 70.sp,
+        fontFamily = FontFamily.Cursive,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = line,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.SansSerif,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .padding(top = 360.dp),
+    )
 }
 
 @Composable
@@ -65,7 +108,14 @@ fun ContactDetails(
     emailid: String,
     modifier: Modifier = Modifier
 ){
-    Column {
+    Column (
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier.padding(
+            bottom = 50.dp,
+            start = 70.dp
+        )
+    ){
         Row (modifier = Modifier.padding(10.dp)){
             Icon(painterResource(id = R.drawable.baseline_phone_24),
                 contentDescription = "Phone Icon",
@@ -73,7 +123,8 @@ fun ContactDetails(
             )
             Spacer(modifier = Modifier.padding(10.dp))
             Text(
-                text = mobileNumber
+                text = mobileNumber,
+                fontSize = 20.sp
             )
         }
         Row (modifier = Modifier.padding(10.dp)){
@@ -83,7 +134,8 @@ fun ContactDetails(
             )
             Spacer(modifier = Modifier.padding(10.dp))
             Text(
-                text = username
+                text = username,
+                fontSize = 20.sp
             )
         }
         Row (modifier = Modifier.padding(10.dp)){
@@ -93,7 +145,8 @@ fun ContactDetails(
             )
             Spacer(modifier = Modifier.padding(10.dp))
             Text(
-                text = emailid
+                text = emailid,
+                fontSize = 20.sp
             )
         }
     }
